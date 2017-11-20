2f6e488a60cf3db861e78507e516a2aa519b5753
Oliver Sosa <cundejo85@gmail.com>
2017-11-20 09:44:47 -0300

    Starting with redux

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Pluralsight admin</h1>
+        <p>React, Redux, and Rect Router in ES6</p>
+        <Link to="about" activeClassName="btn btn-primary btn-lg">Learn more</Link>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
