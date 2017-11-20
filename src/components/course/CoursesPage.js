c6d3d86b603f12ba5a922d34405d6d1428c13fec
Oliver Sosa <cundejo85@gmail.com>
2017-11-20 18:27:42 -0300

    Listing Courses

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,50 +2,18 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-    this.state = {
-      course: {title: ''}
-    };
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
   }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onClickSave(event) {
-    this.props.actions.createCourse(this.state.course);
-  }
-
-  // courseRow(course, index) {
-  //   return <div key={index}>{course.title}</div>;
-  // }
 
   render() {
     return (
       <div className="jumbotron">
         <h1>Courses</h1>
-
-        {this.props.courses.map(
-          (course, index) => <div key={index}>{course.title}</div>
-        )}
-
-        <h2>Add Course</h2>
-
-        <input type="text"
-               onChange={this.onTitleChange}
-               value={this.state.course.title}/>
-
-        <input type="submit"
-               value="Save"
-               onClick={this.onClickSave}/>
-
+        <CourseList courses={this.props.courses}/>
       </div>
     );
   }
@@ -69,3 +37,4 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
+
